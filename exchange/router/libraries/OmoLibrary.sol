bb6f45b520d620b0136595c966e109e8a22b55db
OMO Exchange <125737831+OMO-Exchange@users.noreply.github.com>
2023-02-24 04:48:40 +0800

    Update OmoLibrary.sol

--- exchange/router/libraries/OmoLibrary.sol
+++ exchange/router/libraries/OmoLibrary.sol
@@ -21,7 +21,7 @@ library OmoLibrary {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'0eeece9abfbb4c9b49177d92b23420d9369537a3c347baa8f69923569023f382' // init code hash
             ))));
     }
 
